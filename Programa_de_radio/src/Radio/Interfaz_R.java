051f077cf81855f4e39cce39ac292d38be38ac1c
abbydoag <86386562+abbydoag@users.noreply.github.com>
2023-01-19 09:19:05 -0600

    Add files via upload

---
+++ Programa_de_radio/src/Radio/Interfaz_R.java
@@ -0,0 +1,10 @@
+package Radio;
+
+public interface Interfaz_R {
+	public boolean On_off (boolean status);
+	public static void Radio(String[] args) {
+		System.out.println("BIENVENID@");
+		System.out.println("Encender radio?  (SI/NO)");
+	}
+
+}
